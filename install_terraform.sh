029b5fab67376341c50ec3a2d4c4a3f0fe445db5
juvams <40248496+juvams@users.noreply.github.com>
2020-07-25 15:29:09 -0300

    Update install_terraform.sh
    correção de texto

--- install_terraform.sh
+++ install_terraform.sh
@@ -3,5 +3,5 @@ cd /tmp
 apt install -y unzip
 wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
 unzip terraform_0.11.13_linux_amd64.zip -d /usr/bin/
-echo "Terraform instado"
+echo "Terraform instalado"
 
